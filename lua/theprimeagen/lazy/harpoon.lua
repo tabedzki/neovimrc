35a3bba679ed3997b443b8fde405c55f223cb319
Christian Tabedzki <35670232+tabedzki@users.noreply.github.com>
2024-06-18 19:44:31 -0400

    Removed comments lines

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -19,14 +19,5 @@ return {
             vim.keymap.set("n","<leader>4", function() harpoon:list():select(4) end)
             vim.keymap.set("n","<leader>5", function() harpoon:list():select(5) end)
     end,
-    -- keys = {
-    --   { "<leader>A", function() require("harpoon"):list():add() end, desc = "harpoon file", },
-    --   -- { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-    --   { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
-    --   { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
-    --   { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
-    --   { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
-    --   { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
-    -- },
 }
 
